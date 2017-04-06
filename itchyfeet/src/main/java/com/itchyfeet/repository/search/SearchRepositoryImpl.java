0945b52be9d3f8f4feb5fc9fd4309a26cce861e1
nairabhi95 <nairabhi95@gmail.com>
2017-04-06 08:56:46 +0530

    Add files via upload

---
+++ itchyfeet/src/main/java/com/itchyfeet/repository/search/SearchRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.itchyfeet.repository.search;
+
+import com.itchyfeet.model.User;
+
+public class SearchRepositoryImpl implements SearchRepository{
+
+	public void register(User u){
+		
+	}
+}
